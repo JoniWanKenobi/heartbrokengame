c985df944bf7f440b7aeca12e643ff8514ea84f5
JoniWanKenobi <joniwankenoby@gmail.com>
2018-01-24 14:22:41 +0100

    refactored with puzzle object, all working

--- src/js/game.js
+++ src/js/game.js
@@ -2,6 +2,8 @@
 
 function Game(app, createFunction, puzzleSize, puzzleWidth, imageURL) {
     var self = this;
+    self.createFunction = createFunction;
+
     //State
     self.finished = false;
 
@@ -13,57 +15,28 @@ function Game(app, createFunction, puzzleSize, puzzleWidth, imageURL) {
 
     //Baseline DOME elements
     self.app = app;
-    self.wrapperElement = createFunction('<div class="wrapper"></div>');
-    self.puzzleCardElement = createFunction('<div class="puzzle card"></div>');
-    self.cardBodyElement = createFunction('<div class="card-body"></div>'); 
-    self.boardElement = createFunction('<div class="board"></div>');
-    self.patElement = createFunction('<div class="pat"></div>');
-    self.cardElement = createFunction('<div class="card"></div>');
-    self.boardCardBodyElement = createFunction('<div class="card-body"></div>');   
-    
-    //Data
-    self.defaultData = {
-        messages: [['Sheng', 'M', 'Alicante', 'Never allow someone to be your priority while allowing yourself to be their option.', 5],
-                    ['Josep', 'M', 'Barcelona', 'An invisible red thread connects those who are destined to meet, regardless of time, place, or circumstance. The thread may stretch or tangle, but will never break.', 3, 'ancient Chinese Proverb'],
-                    ['Andrea', 'M', 'Lisbon', 'What matters in life is not to predict the dangers of travels; is to have them made.', 4, 'Agostinho da Silva'],
-                    ['Stephanie', 'F', 'Innsbruck', 'Everything is going to be ok in the end, if it\'s not okay it\'s not the end', 5 ],
-                    ['Stephanie', 'F', 'Innsbruck', 'f you are brave enough to say goodbye, life will reward you with a new hello', 5 ]
-    };
-    self.messages = [];
-    self.pieces = [];
-
-    self.createMessagesArray(self.defaultData.messages);    
-    self.createPiecesArray(self.puzzleWidth, self.cellWidth);  
+    self.wrapperElement = createFunction('<div class="wrapper"></div>');   
+      
 }
 
 
-    Game.prototype.createMessagesArray = function(arr){
-        var self = this;
-        arr.forEach(element){
-            var message = new CheerUpMessage.apply(null, element)
-            self.messages.push(message);
-        };
-        self.messages.sort(function(a,b){
-            return a[4] - b[4]; //sorts by ranking
-        });
-    };
-
-    Game.prototype.createPiecesArray = function(puzzleSize, cellSize){
-        var self = this;
-
-    }
-  
-  Game.prototype.makePuzzle = function(){
+Game.prototype.build = function(){
     var self = this;
-    for(var i=1; i<=self.puzzleSize; i++){
-        var cellElement = document.createElement('div');
-        cellElement.setAttribute('id', 'drop-'+i);
-        cellElement.setAttribute('class', 'drop');
-        self.cardBodyElement.appendChild(cellElement);
-    }
+    var puzzleElement = self.buildPuzzle();
+    self.wrapperElement.appendChild(puzzleElement);
+  //   self.wrapperElement.appendChild(self.puzzleCardElement);
+  //   self.wrapperElement.appendChild(self.boardElement);
+    app.appendChild(self.wrapperElement);
+    dragula([document.getElementById("el-1"), document.getElementById("drop-1")].on('drop', self.getTurn), {revertOnSpill: true});
+}  
 
-    self.puzzleCardElement.appendChild(self.cardBodyElement);
-  }  
+
+Game.prototype.buildPuzzle = function(){
+    var self = this;
+    var puzzle = new Puzzle(self.createFunction, self.puzzleSize);
+    return puzzle.makePuzzle(); //Returns a puzzle grid
+}
+
 
   Game.prototype.getTurn = function(){
     var self = this;
@@ -71,14 +44,7 @@ function Game(app, createFunction, puzzleSize, puzzleWidth, imageURL) {
     var newPiece = self.getPiece();
   }
   
-  Game.prototype.build = function(){
-      var self = this;
-      self.makePuzzle();
-      self.wrapperElement.appendChild(self.puzzleCardElement);
-      self.wrapperElement.appendChild(self.boardElement);
-      app.appendChild(self.wrapperElement);
-      dragula([document.getElementById("el-1"), document.getElementById("drop-1")].on('drop', self.getTurn), {revertOnSpill: true});
-  }  
+
 
   Game.prototype.destroy = function () {
     var self = this;  
---
+++ src/js/puzzle.js
@@ -0,0 +1,21 @@
+function Puzzle(createFunction, puzzleSize){
+    var self = this;
+    self.puzzleSize = puzzleSize;
+
+    //Baseline DOM elements
+    self.puzzleCardElement = createFunction('<div class="puzzle card"></div>');
+    self.cardBodyElement = createFunction('<div class="card-body"></div>');
+}
+
+
+Puzzle.prototype.makePuzzle = function(){
+    var self = this;
+    for(var i=1; i<=self.puzzleSize; i++){
+        var cellElement = document.createElement('div');
+        cellElement.setAttribute('id', 'drop-'+i);
+        cellElement.setAttribute('class', 'drop');
+        self.cardBodyElement.appendChild(cellElement);
+    }
+    self.puzzleCardElement.appendChild(self.cardBodyElement);
+    return self.puzzleCardElement;
+  }
